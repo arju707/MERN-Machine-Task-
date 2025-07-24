ce0c51f07c2732bbece8d2013f6c98d2baa18798
arjun vinod <arjunvinod707@gmail.com>
2025-07-24 12:23:55 +0530

    button color changed

--- client/src/components/AddProduct.jsx
+++ client/src/components/AddProduct.jsx
@@ -213,7 +213,7 @@ const AddProduct = () => {
 
         <button
           type="submit"
-          className="bg-green-600 text-white px-4 py-2 rounded"
+          className="bg-yellow-400 text-white px-4 py-2 rounded"
         >
           Submit
         </button>
